8a5340de61e28e5de0efca412329819170860b80
Samuel Gillispie II <spgillispie2@gmail.com>
2026-01-21 11:45:15 -0500

    Add some simple name validation to the schema

--- spgill/backup/schema.py
+++ spgill/backup/schema.py
@@ -1,8 +1,34 @@
 ### stdlib imports
+import re
 import typing
 
 ### vendor imports
 import pydantic
+
+
+# A modification of RFC 1123 (used by k8s) substituting hyphen for underscore as that is more Pythonic
+name_pattern = re.compile(r"^[a-z0-9]([_a-z0-9]*[a-z0-9])?$")
+name_max_length = 63
+
+
+def str_is_valid_name(value: str) -> str:
+    if not name_pattern.match(value):
+        raise ValueError(
+            f"'{value}' does not confirm to regex '{name_pattern.pattern}'"
+        )
+    if len(value) > name_max_length:
+        raise ValueError(
+            f"'{value}' is more than {name_max_length} characters"
+        )
+    return value
+
+
+def dict_keys_are_valid_names(
+    value: dict[str, typing.Any],
+) -> dict[str, typing.Any]:
+    for key in value:
+        str_is_valid_name(key)
+    return value
 
 
 class BackupLocation(pydantic.BaseModel):
@@ -30,6 +56,10 @@ class BackupPolicy(pydantic.BaseModel):
 
 
 class BackupProfile(pydantic.BaseModel):
+    id: typing.Annotated[
+        typing.Optional[str], pydantic.AfterValidator(str_is_valid_name)
+    ] = None
+
     policy: typing.Optional[str] = None
     hostname: typing.Optional[str] = None
     archive_name: typing.Optional[str] = None
@@ -62,9 +92,18 @@ class ArchiveConfiguration(pydantic.BaseModel):
 
 
 class RootBackupConfiguration(pydantic.BaseModel):
-    locations: dict[str, BackupLocation]
-    policies: dict[str, BackupPolicy]
-    profiles: dict[str, BackupProfile]
+    locations: typing.Annotated[
+        dict[str, BackupLocation],
+        pydantic.AfterValidator(dict_keys_are_valid_names),
+    ]
+    policies: typing.Annotated[
+        dict[str, BackupPolicy],
+        pydantic.AfterValidator(dict_keys_are_valid_names),
+    ]
+    profiles: typing.Annotated[
+        dict[str, BackupProfile],
+        pydantic.AfterValidator(dict_keys_are_valid_names),
+    ]
 
     global_profile: typing.Optional[BackupProfile] = None
     archive: typing.Optional[ArchiveConfiguration] = None
